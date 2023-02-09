09ce869ab881a7bc7b170a38b06ef158a86da46a
Amber <wenjuasnhen29@gmail.com>
2023-02-09 14:11:04 +0800

    详情页

---
+++ src/components/SwiperList/index.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script>
+import Swiper from "swiper/swiper-bundle.min";
+import "swiper/swiper-bundle.min.css";
+
+export default {
+  name: "Swiper"
+};
+</script>
+
+<style scoped>
+
+</style>
